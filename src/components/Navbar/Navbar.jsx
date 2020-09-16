a31e2d8f56d4fea4a1da447787e06b5b47da1df0
SHAH0001 <vlad-t-97@mail.ru>
2020-09-16 19:02:57 +0600

    add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,35 @@
 import React from "react";
-import "./Header.scss";
+import "./Navbar.scss";
 
 const Navbar = () => {
-    // return (
-        
-    // );
+    return (
+        <nav class="nav">
+            <div class="nav-section">
+                <h3 class="nav-title">Компания</h3>
+                <ul class="nav-items">
+                    <li><a href="#">Главная</a></li>
+                    <li><a href="#">Запросы</a></li>
+                    <li><a href="#">Календарь</a></li>
+                    <li><a href="#">База знаний</a></li>
+                    <li><a href="#">Новости</a></li>
+                    <li><a href="#">Задачи</a></li>
+                    <li><a href="#">Чаты</a></li>
+                </ul>
+            </div>
+            <div class="nav-section">
+                <h3 class="nav-title">Администратор</h3>
+                <ul class="nav-items">
+                    <li><a href="#">Статистика компаний</a></li>
+                    <li><a href="#">Структура компаний</a></li>
+                    <li><a href="#">Сотрудники</a></li>
+                    <li><a href="#">База знаний</a></li>
+                    <li><a href="#">Новости</a></li>
+                    <li><a href="#">Настройки</a></li>
+                    <li><a href="#">Служба поддержки</a></li>
+                </ul>
+            </div>
+        </nav>        
+    );
 }
+
+export default Navbar;
