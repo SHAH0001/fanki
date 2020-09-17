3a3405b9b9d01968bfb74251a1318f7a80397b87
SHAH0001 <vlad-t-97@mail.ru>
2020-09-17 12:31:43 +0600

    icons added to navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.scss';
 import './scss/vars.scss';
 import Header from './components/Header/Header.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
+import MainContent from './components/MainContent/MainContent.jsx';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <div className="container">
+        <div className="container-content">
+          <Navbar />
+          <MainContent />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/MainContent/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./MainContent.scss";
+
+const MainContent = () => {
+    return (
+        <div>Content!</div>
+    );
+}
+
+export default MainContent;
